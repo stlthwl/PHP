53a92536a646dd810ec1744b8c60cfbf8fba084d
stlthwl <stlthwl92@mail.ru>
2023-12-15 10:19:56 +0300

    third commit

--- lessons/index.php
+++ lessons/index.php
@@ -2,7 +2,7 @@
     $title = "Главная";
     require "header.php";
 ?>
-<div style="overflow: auto; height: 100%;">
+<div class="main-container">
     <div class="container" style="height: 100%;">
         <h1>Главная</h1>
     </div>
--- lessons/info.php
+++ lessons/info.php
@@ -1,28 +1,32 @@
 <?php
-    $title = "Обратная связь";
-    require "header.php";
+$title = "Обратная связь";
+require "header.php";
 ?>
-<div class="container" style="height: 100%;">
-    <h1>Info</h1>
+<div class="main-container">
+    <div class="container" style="height: 100%;">
+        <h1>Info</h1>
 
-    <form action="chek_post.php" method="post">
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">email</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Введите email">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">login</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Введите имя пользователя">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">password</label>
-            <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Введите пароль">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Сообщение</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Введите текст сообщения"></textarea>
-        </div>
-    </form>
+        <form action="chek_post.php" method="post">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">email</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Введите email">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">login</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1"
+                       placeholder="Введите имя пользователя">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">password</label>
+                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Введите пароль">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Сообщение</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                          placeholder="Введите текст сообщения"></textarea>
+            </div>
+        </form>
+    </div>
 </div>
 
 <?php
